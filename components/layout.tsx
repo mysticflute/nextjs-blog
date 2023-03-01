f34d8255f3247dbac6ae824bb8ebcb605fce1d07
Nathan McWilliams <mysticflute@users.noreply.github.com>
2023-03-01 00:21:24 -0500

    remove profile image

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,15 +1,18 @@
-import Head from 'next/head';
-import Image from 'next/image';
-import styles from './layout.module.css';
-import utilStyles from '../styles/utils.module.css';
-import Link from 'next/link';
+import Head from "next/head";
+import Image from "next/image";
+import styles from "./layout.module.css";
+import utilStyles from "../styles/utils.module.css";
+import Link from "next/link";
 
-const name = 'Nathan';
-export const siteTitle = 'Next.js Sample Website';
+const name = "Nathan";
+export const siteTitle = "Next.js Sample Website";
 
-export default function Layout({ children, home }: {
-  children: React.ReactNode
-  home?: boolean
+export default function Layout({
+  children,
+  home,
+}: {
+  children: React.ReactNode;
+  home?: boolean;
 }) {
   return (
     <div className={styles.container}>
@@ -22,7 +25,7 @@ export default function Layout({ children, home }: {
         <meta
           property="og:image"
           content={`https://og-image.vercel.app/${encodeURI(
-            siteTitle,
+            siteTitle
           )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
         />
         <meta name="og:title" content={siteTitle} />
@@ -31,14 +34,14 @@ export default function Layout({ children, home }: {
       <header className={styles.header}>
         {home ? (
           <>
-            <Image
+            {/* <Image
               priority
               src="/images/profile.png"
               className={utilStyles.borderCircle}
               height={144}
               width={144}
               alt=""
-            />
+            /> */}
             <h1 className={utilStyles.heading2Xl}>{name}</h1>
           </>
         ) : (
